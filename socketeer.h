d808fe02b58d9d5b737635a0c8615febbdf41d3b
larrabyte <larrabyte@protonmail.ch>
2019-11-08 18:51:40 +1100

    Move more functions to header file
    commoninit(), serverinit(), clientinit(), fetchinput(), safealloc() and exitsock() have been moved to the Socketeer header file.

--- socketeer.h
+++ socketeer.h
@@ -1,6 +1,10 @@
 #pragma once
 
 #include <winsock2.h>
+#include <ws2tcpip.h>
+#include <stdio.h>
+
+#define TERMINALMAX 4096   // Maximum characters inside a terminal.
 
 typedef struct packet_ts {  // Struct typedef for a Socketeer packet.
     unsigned char *data;
@@ -19,3 +23,94 @@ void *safealloc(void *memory, size_t size);
 SOCKET serverinit(setupdata_ts sockinfo);
 void clientinit(setupdata_ts sockinfo);
 void fetchinput(char *buffer);
+
+setupdata_ts commoninit(char **argv, int type) {
+    struct addrinfo *result = NULL, hints;
+    memset(&hints, 0, sizeof(hints));
+    hints.ai_socktype = SOCK_STREAM;
+    hints.ai_protocol = IPPROTO_TCP;
+    int retc;
+
+    if(type == 0) {
+        hints.ai_flags = AI_PASSIVE;                                // Integer types:
+        hints.ai_family = AF_INET;                                  // 0 is for server
+        retc = getaddrinfo(NULL, argv[2], &hints, &result);         // 1 is for client
+    } else if(type == 1) {
+        hints.ai_family = AF_UNSPEC;
+        retc = getaddrinfo(argv[2], argv[3], &hints, &result);
+    }
+
+    if(retc != 0) {
+        fprintf(stderr, "Socketeer failed to get address info.\n");
+        exitsock(NULL, INVALID_SOCKET, 1);
+    }
+
+    SOCKET commonsock = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
+    if(commonsock == INVALID_SOCKET) {
+        fprintf(stderr, "Socketeer failed to create socket, code %d.\n", WSAGetLastError());
+        exitsock(result, commonsock, 1);
+    }
+
+    setupdata_ts retdata = {result, commonsock};
+    return retdata; // Return a setupdata_ts struct.
+}
+
+SOCKET serverinit(setupdata_ts sockinfo) {
+    int retc = bind(sockinfo.socketeer, sockinfo.result->ai_addr, (int) sockinfo.result->ai_addrlen);
+    if(retc == SOCKET_ERROR) {
+        fprintf(stderr, "Socketeer failed to create/bind socket, code %d.\n", WSAGetLastError());
+        exitsock(sockinfo.result, sockinfo.socketeer, 1);
+    }
+
+    freeaddrinfo(sockinfo.result);  // Free result struct, we don't need it anymore.
+    sockinfo.result = NULL;         // Just to make sure, set pointer to NULL.
+
+    retc = listen(sockinfo.socketeer, SOMAXCONN);   // Try to listen on the socket.
+    if(retc == SOCKET_ERROR) {                      // If a listening attempt on the socket fails?
+        fprintf(stderr, "Socketeer failed to listen on socket, code %d.\n", WSAGetLastError());
+        exitsock(sockinfo.result, sockinfo.socketeer, 1);
+    }
+
+    SOCKET clients = accept(sockinfo.socketeer, NULL, NULL);
+    if(clients == INVALID_SOCKET) {
+        fprintf(stderr, "Socketeer failed to accept a connection, code %d.\n", WSAGetLastError());
+        exitsock(sockinfo.result, clients, 1);
+    }
+
+    return clients; // Return the connected socket.
+}
+
+void clientinit(setupdata_ts sockinfo) {
+    int retc = connect(sockinfo.socketeer, sockinfo.result->ai_addr, (int) sockinfo.result->ai_addrlen);
+    if(retc == SOCKET_ERROR) {
+        fprintf(stderr, "Socketeer failed to connect, code %d.\n", WSAGetLastError());
+        exitsock(sockinfo.result, sockinfo.socketeer, 1);
+    }
+}
+
+void fetchinput(char *buffer) {
+    memset(buffer, 0, TERMINALMAX);  // Zero out the buffer.
+    size_t buffersize = 0;           // Cache size of actual data.
+    int onechar;
+
+    while((onechar = fgetc(stdin)) != EOF && onechar != '\n') buffer[buffersize++] = (char) onechar;
+}
+
+void *safealloc(void *memory, size_t size) {
+    if(memory == NULL) memory = malloc(size);
+    else memory = realloc(memory, size);
+
+    if(memory == NULL) {
+        fprintf(stderr, "Socketeer ran out of memory.\n");
+        exitsock(NULL, INVALID_SOCKET, 1);
+    }
+
+    return memory;
+}
+
+void exitsock(struct addrinfo *result, SOCKET socket, int code) {
+    if(socket != INVALID_SOCKET) closesocket(socket);
+    if(result != NULL) freeaddrinfo(result);
+    WSACleanup();
+    exit(code);
+}
